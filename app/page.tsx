697e11ba0eed402937d6be30a7c6dd3440c2f03b
evan-walter <ewpw5678@gmail.com>
2023-09-04 17:32:10 -0700

    Add font

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <main className='container w-full'>
-      <h1 className='mt-6 text-4xl text-center font-semibold'>
-        Welcome to{' '}
-        <span className='text-primary'>the Emotional Health App!</span>
+      <h1 className='mt-6 text-4xl text-center font-bold'>
+        Welcome to the <span className='text-primary'>Emotional Health</span>{' '}
+        App!
       </h1>
     </main>
   );
